6e47262f69b02e21437bed437d334cd60dc0aa98
gustavo <gustavosecardovelli@gmail.com>
2025-07-01 16:14:24 -0300

    att

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Corrigir encoding e ativar diagnósticos globais
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.offsetEncoding = { "utf-8" }
+
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "strict",
+              diagnosticMode = "workspace", -- mostra problemas em todos os arquivos
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+
+      lspconfig.ruff_lsp.setup({
+        capabilities = capabilities,
+      })
+    end,
+  },
+}
--- lua/plugins/python_format.lua
+++ lua/plugins/python_format.lua
@@ -4,11 +4,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        python = { "ruff_format", "isort", "black" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        timeout_ms = 1000,
+        python = { "ruff_format", "isort" },
       },
     },
   },
