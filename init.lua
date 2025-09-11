1f2ee0e4f305b1d435d9a4a702496a8a2cdaf090
gustavo <gustavosecardovelli@gmail.com>
2025-09-11 17:37:58 -0300

    att

--- init.lua
+++ init.lua
@@ -1,2 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+vim.g.snacks_loaded = 1
 require("config.lazy")
+
+-- se Snacks for carregado pelo LazyVim, desative assim:
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
